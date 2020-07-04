31c735bded4adc58ce3f3a97555e62a8ec3b744d
babylonwanderer <17422425+babylonwanderer@users.noreply.github.com>
2020-07-04 17:04:43 +0100

    Update scan.py

--- scan/scan.py
+++ scan/scan.py
@@ -1,22 +1,15 @@
 import discord
-from discord.ext import commands
 import asyncio
+from redbot.core import commands
 
 class Scan:
     """Scans the user mentioned """
 
-    def __init__(self, bot):
-        self.bot = bot
-
     @commands.command()
-    async def scan(self, user : discord.Member):
+    async def scan(self, ctx, {0.mention:Member}):
         """Scans the mentioned user.. """
 
         #Your code will go here
-        await self.bot.say("takes <:tricorder:340431262613241866> readings of " + user.mention + ", and conducts an analysis the results.")
+        await self.bot.say("takes <:tricorder:340431262613241866> readings of " + {0.mention:Member} + ", and conducts an analysis the results.")
         await asyncio.sleep(10)
         await self.bot.say("<:tricorder:340431262613241866> readings indicate it is a lifeform, but I have NEVER see anything even remotely similar!!!. :confused:")
-
-
-def setup(bot):
-    bot.add_cog(Scan(bot))
